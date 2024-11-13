d6645492c063ab6d6f55b8ecf734cdd73b13a926
Marco-Guarnera <Darkaquon93@gmail.com>
2024-11-13 16:55:13 +0100

    Updated DatabaseSeeder.php in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder {
     public function run() : void {
         $this->call([
             TypeSeeder::class,
-            ProjectSeeder::class]);
+            ProjectSeeder::class,
+            TechnologySeeder::class]);
     }
 }
